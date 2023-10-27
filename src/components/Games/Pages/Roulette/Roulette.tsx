9ac276c515a75900307a88bb580da14680826ff0
thunder-splash <111632585+thunder-splash@users.noreply.github.com>
2023-10-27 21:50:03 +0300

    Рулетка начата

--- src/components/Games/Pages/Roulette/Roulette.tsx
+++ src/components/Games/Pages/Roulette/Roulette.tsx
@@ -9,22 +9,20 @@ export default function Roulette() {
     return (
         <div className={`${styles.main}`}>
             <div className={styles.upper}>
-                <div className={styles.jackcollumn}>
+                <div className={styles.roulcollumn}>
                     <div className={`${styles.game}`}>
                         <div className={`${styles.header}`}>
-                            <img src="/Ellipse%2087.svg" alt="light" className={styles.jackpot__light1}></img>
-                            <Image src="/jackpoticon.svg" alt="Jackpot Icon" width={20} height={20}/>
+                            <img src="/Ellipse%2087.svg" alt="light" className={styles.roulette__light1}></img>
+                            <Image src="/rouletteicon.svg" alt="Roulette Icon" width={20} height={20}/>
                             <h2>ROULETTE</h2>
                         </div>
-                        <BetContainer/>
                         <div className={styles.footer}>
-                            <button onClick={() => console.log('Play button clicked!')} className={styles.play__button}>
-                                Play
-                            </button>
+                            <BetContainer/>
                         </div>
                     </div>
                     <GameSecurity/>
                 </div>
+
             </div>
         </div>
     );
