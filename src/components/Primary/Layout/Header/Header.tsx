d14544ab17d57347e572b014a55cb713d46d0ed6
thunder-splash <111632585+thunder-splash@users.noreply.github.com>
2023-11-10 17:15:35 +0300

    50% адаптива хедера, - остальное после того как проснусь

--- src/components/Primary/Layout/Header/Header.tsx
+++ src/components/Primary/Layout/Header/Header.tsx
@@ -11,6 +11,11 @@ export const Header = () => {
     const [isAuth, setIsAuth] = useState(true);
     const [showSignUpForm, setShowSignUpForm] = useState(false);
     const [isLogin, setIsLogin] = useState(false);
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+    const toggleDropdown = () => {
+        setIsDropdownOpen(!isDropdownOpen);
+    };
 
     const toggleSignUpForm = () => {
         setShowSignUpForm(!showSignUpForm);
@@ -102,6 +107,9 @@ export const Header = () => {
                             showLogin={isLogin}
                         />
                     )}
+                    <button className={styles.burger} onClick={toggleDropdown}>
+                        <img src={isDropdownOpen ? "/burger_closer.svg" : "/burger_on_header.svg"} alt="burger"/>
+                    </button>
                 </div>
             )}
         </header>
